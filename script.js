4df3894e0f3ea742b79a3277662ed92432b296b4
asavin12 <motnuatraitimhp@gmail.com>
2025-07-08 19:59:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -437,7 +437,7 @@ async function checkGrammar() {
     grammarResult.innerHTML = "";
 
     try {
-        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${geminiApiKey}`, {
+        const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${geminiApiKey}`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
